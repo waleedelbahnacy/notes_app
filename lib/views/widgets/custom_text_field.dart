5c9835aa13c1b93ab3cfb3c1af0366b3f97fbb5e
waleedelbahnacy <waleedelbahnacy@gmail.com>
2024-12-30 01:02:41 +0200

    edit note done

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -3,15 +3,17 @@ import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
   const CustomTextField(
-      {super.key, required this.hint, this.maxLines = 1, this.onSaved});
+      {super.key, required this.hint, this.maxLines = 1, this.onSaved, this.onChanged});
 
   final String hint;
   final int maxLines;
+final Function(String)? onChanged;
 
   final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onChanged:onChanged ,
       onSaved: onSaved,
       validator: (value) {
         if (value?.isEmpty ?? true) {
